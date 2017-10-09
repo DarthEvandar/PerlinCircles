2617a20fc7a9396995bb4b3d247e6935236bccda
Anders Sundheim <andersundheim@gmail.com>
2017-10-09 09:25:02 -0500

    hardware acceleratio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+alert("Disable Hardware Acceleration to make this run smoothly")
 t=0;
 h=200;
 var canvas = document.getElementById("canvas");
@@ -28,29 +29,19 @@ for (var x = (canvas.width/2)-h; x < (canvas.width/2)+h; x++) {
       	b = 150;
       }
       else if(value<.5){
-      	//ctx.strokeStyle  = "rgb(0,0,255)";
       	r = 0;
       	g = 0;
       	b = 255;
       }
       else if(value<.6){
-      	//ctx.strokeStyle = "rgb(255,255,0)"
       	r = 637*value;
       	g = 637*value;
       	b = 0;
-      }/*else if(value<.7){
-      	//ctx.strokeStyle = "rgb(0,255,0)";	
-      	r = 0;
-      	g = 255;
-      	//.7x = 255
-      	b = 0;
-      }*/else if(value<.8){
-      	//ctx.strokeStyle = "rgb(0,150,0)";
+      }else if(value<.8){
       	r = 0;
       	g = 450-(318*value);
       	b = 0;
       }else{
-      	//ctx.strokeStyle = "rgb(175,175,175)"
       	r = 0;
       	g = 200;
       	b = 0;
@@ -60,14 +51,10 @@ for (var x = (canvas.width/2)-h; x < (canvas.width/2)+h; x++) {
       d[2]   = b;
       d[3]   = 255;
       ctx.putImageData( id, x, y ); 
-     // ctx.putImageData(id,x+1,y)
-    	/*ctx.beginPath();
-    	ctx.arc(x,y,Math.abs(value)*3,0,Math.PI*2);
-    	ctx.stroke();*/
     }
 }
 
-    /*t+=.001;
+   /* t+=.001;
     for (var x = (canvas.width/2)-h; x < (canvas.width/2)+h; x++) {
       for (var y = (canvas.height/2)-h; y < (canvas.height/2)+h; y++) {
         var value = noise.simplex2(x / 100, y / 100);
@@ -89,7 +76,7 @@ for (var x = (canvas.width/2)-h; x < (canvas.width/2)+h; x++) {
         ctx.stroke();
        }
    }*/
-	/*ctx.fillRect(0,0,canvas.width,canvas.height);
+/*	ctx.fillRect(0,0,canvas.width,canvas.height);
   t+=0.01;
   for (var x = 0; x < canvas.width; x+=canvas.width/h) {
   		for (var y = 0; y < canvas.height; y+=canvas.width/h) {
@@ -114,45 +101,24 @@ for (var x = (canvas.width/2)-h; x < (canvas.width/2)+h; x++) {
   		}
   	}*/
   //ctx.fillRect(0,0,canvas.width,canvas.height);
- /* t+=0.1;
+  /*t+=0.1;
   for (var x = 0; x < 400; x++) {
   		for (var y = 0; y < 400; y++) {
-		var r;
-		var g;
-		var b;
 		var value = noise.simplex3(x / 100, y / 100,t);
 		if(value<.3){
-      	//ctx.strokeStyle  = "rgb(0,0,255)";
-      	r = 0;
-      	g = 0;
-      	b = 255;
+      	ctx.strokeStyle  = "rgb(0,0,255)";
       }
       else if(value<.4){
-      	//ctx.strokeStyle = "rgb(255,255,0)"
-      	r = 255;
-      	g = 255;
-      	b = 0;
+      	ctx.strokeStyle = "rgb(255,255,0)"
       }else if(value<.7){
-      	//ctx.strokeStyle = "rgb(0,255,0)";	
-      	r = 0;
-      	g = 255;
-      	b = 0;
+      	ctx.strokeStyle = "rgb(0,255,0)";	
       }else if(value<.9){
-      	//ctx.strokeStyle = "rgb(0,150,0)";
-      	r = 0;
-      	g = 150;
-      	b = 0;
+      	ctx.strokeStyle = "rgb(0,150,0)";
       }else{
-      	//ctx.strokeStyle = "rgb(175,175,175)"
-      	r = 175;
-      	g = 175;
-      	b = 175;
+      	ctx.strokeStyle = "rgb(175,175,175)"
       }
-      d[0]   = r;
-      d[1]   = g;
-      d[2]   = b;
-      d[3]   = 255;
-      ctx.putImageData( id, x, y ); 
+      ctx.arc(x,y,Math.abs(value)*10,0,Math.PI*2);
+    ctx.stroke();
   		}
   	}*/
   /*t+=0.1;
